b4dc8703a1515915d10d2534e02f9771bfec99db
Thomas <nyumat18@gmail.com>
2023-06-30 17:26:26 -0700

    Auth Page Finished

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,7 +1,27 @@
 const Auth = () => {
   return (
     <>
-        
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse p-48">
+          <div className="text-center lg:text-left m-12">
+            <h1 className="text-5xl font-bold">Begin Your Journey with SoundConductor</h1>
+            <p className="py-6">Put instructions here</p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Token</span>
+                </label>
+                <input type="text" placeholder="Enter Your Token Here" className="input input-bordered" />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Enter SoundConductor</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -4,12 +4,12 @@ const Landing = () => {
   const navigate = useNavigate()
   return (
     <>
-      <div className="hero min-h-screen bg-base-200">
+      <div className="hero min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-lg">
-            <h1 className="text-5xl font-bold">Welcome To SoundUI</h1>
+            <h1 className="text-5xl font-bold">Welcome To SoundConductor</h1>
             <p className="py-6 max-w-lg">
-              SoundUI is a smart sound classification tool that allows you to make your life easier.
+              SoundConductor is a smart sound classification tool that allows you to make your life easier.
             </p>
             <button className="btn btn-primary"
               onClick={() => {
